b9cc894a062321adc9e43a31bb2f1979e5034802
aun1414 <abbasaun1414@gmail.com>
2024-01-11 21:03:07 -0500

    API done

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+
+import Header from './components/header';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <div className="container">
+      <h1> Hello World</h1>
+    </div>
+    </>
+  );
+}
+
+export default App;
