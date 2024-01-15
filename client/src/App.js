e9a29a22d1a0153911cbbcb703303602ae2d4c06
aun1414 <abbasaun1414@gmail.com>
2024-01-15 16:10:05 -0500

    Data fetched

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
 
 import Header from './components/header';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import Clients from '../src/components/Clients';
+import Projects from './components/Projects';
+import AddClientModal from './components/AddClientModal';
+const client= new ApolloClient({
+  uri:'http://localhost:5000/graphql',
+  cache:new InMemoryCache(),
+
+});
 
 function App() {
   return (
     <>
+    <ApolloProvider client={client}>
     <Header/>
     <div className="container">
-      <h1> Hello World</h1>
+      <AddClientModal/>
+      <Projects/>
+      <Clients/>
     </div>
+    </ApolloProvider>
     </>
   );
 }
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const {graphqlHTTP}= require('express-graphql');
 const connectDB=require('./config/db')
 const schema = require('./schema/schema');
+const cors=require('cors')
 const port=process.env.PORT || 5000
 
 
@@ -10,6 +11,8 @@ const port=process.env.PORT || 5000
 const app= express();
 
 connectDB();
+
+app.use(cors());
 app.use('/graphql',graphqlHTTP({
     schema,
     graphiql:process.env.NODE_ENV==='development',
